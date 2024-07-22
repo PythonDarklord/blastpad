091f0a6772a7e63a2aff636b3cfcf800bf99395f
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-07-22 14:41:11 -0700

    fix speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import "@/styles/globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-  <SpeedInsights/>
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
