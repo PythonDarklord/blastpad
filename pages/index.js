c76dc89bd46bbde67df01b9ee84f3828aa03c447
PythonDarklord <64159426+PythonDarklord@users.noreply.github.com>
2024-07-21 19:38:45 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -110,7 +110,7 @@ export default function Home() {
                 <ul id="favoritesList" style={"list-style-type: none"}>
                   {favorites.map((item) => (
                     <li>
-                      <a href={item.url}>{item.name}</a>
+                      <a href={item.url} type="_blank">{item.name}</a>
                     </li>
                   ))}
                 </ul>
