2e59a9bc05fcf51d308e96abba552e8c04f265b6
Python Darklord <pythondarklord@gmail.com>
2024-07-25 10:18:26 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -213,7 +213,7 @@ export default function Home() {
                     <td>
 
                     </td>
-                    </tr>
+                     </tr>
                     ))}
               </table>
               </div>
