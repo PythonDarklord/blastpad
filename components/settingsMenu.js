2b2daca948a98388af6a72dec06ac4b7938f8d8b
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-07-25 21:24:49 -0700

    all colors

--- components/settingsMenu.js
+++ components/settingsMenu.js
@@ -1,49 +1,44 @@
 import styles from "@/styles/Popup.module.css";
 
-
 export default function SettingsMenu({ closeMethod, applyMethod }) {
-    return (
-        <div className={styles.fullscreen}>
-            <div className={styles.popup}>
-                <button onClick={closeMethod} className={styles.close}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <line x1="18" y1="6" x2="6" y2="18"></line>
-                        <line x1="6" y1="6" x2="18" y2="18"></line>
-                    </svg>
-                </button>
-                <form className={styles.form} onSubmit={(e) => applyMethod(e)}>
-                    <h2>Settings</h2>
-                    <div>
-                        <label htmlFor="favoritesColor">Favorites Color: </label>
-                        <select id="favoritesColor" name="favoritesColor" className={styles.dropdown}>
-                            <option value="yellow">Yellow</option>
-                            <option value="crimson">Crimson</option>
-                            <option value="brown">Brown</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="emailColor">Email Color: </label>
-                        <select id="emailColor" name="emailColor" className={styles.dropdown}>
-                            <option value="yellow">Yellow</option>
-                            <option value="crimson">Crimson</option>
-                            <option value="brown">Brown</option>
-                        </select>
-                    </div>
-                    <div>
-                        <button>Apply</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.fullscreen}>
+      <div className={styles.popup}>
+        <button onClick={closeMethod} className={styles.close}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </button>
+        <form className={styles.form} onSubmit={(e) => applyMethod(e)}>
+          <h2>Settings</h2>
+          <div>
+            <label htmlFor="favoritesColor">Favorites Color: </label>
+            <input
+              type="color"
+              id="favoritesColor"
+              name="favoritesColor"
+            ></input>
+          </div>
+          <div>
+            <label htmlFor="emailColor">Email Color: </label>
+            <input id="emailColor" name="emailColor" type="color"></input>
+          </div>
+          <div>
+            <button>Apply</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
