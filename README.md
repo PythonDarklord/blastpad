c31ad0c6822e8d8385df0bcae5b1e961f7d2622f
Python Darklord <pythondarklord@gmail.com>
2024-08-11 17:15:28 -0700

    Changes

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # BlastPad
 
 ## TODOS:
+
 - [x] Save Favorites between sessions
 - [x] Save emails between sessions
 - [ ] Add To-Do Functionality
     - [x] Able to Add Items
     - [ ] Able to mark items as done
-- [X] Add Notes Funtionality
+- [X] Add Notes Functionality
     - [X] Notes Saving
     - [x] Basic Notes Functionality
 - [ ] Make Colors Save
--- components/settingsMenu.js
+++ components/settingsMenu.js
@@ -1,49 +1,72 @@
 import styles from "@/styles/Popup.module.css";
+import {useEffect, useState} from "react";
 
+const updatePanelColors = (e, panelColors, setPanelColors) => {
+    const id = e.target.id;
+    let value;
+    const updatedPanelColors = {
+        ...panelColors,
+        [id]: value,
+    };
+}
 
-export default function SettingsMenu({color, closeMethod, applyMethod }) {
-  return (
-    <div className={styles.fullscreen}>
-      <div className={styles.popup}>
-        <button onClick={closeMethod} className={styles.close}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="18" y1="6" x2="6" y2="18"></line>
-            <line x1="6" y1="6" x2="18" y2="18"></line>
-          </svg>
-        </button>
-        <form className={styles.form} onSubmit={(e) => applyMethod(e)}>
-          <h2>Settings</h2>
-          <div>
-            <label htmlFor="favoritesColor">Favorites Color: </label>
-            <input className={styles.colorInput} type="color" id="favoritesColor" name="favoritesColor" defaultValue={"#F6D454"}></input>
-          </div>
-          <div>
-            <label htmlFor="emailColor">Email Color: </label>
-            <input className={styles.colorInput} id="emailColor" name="emailColor" type="color" defaultValue={"#a54040"}></input>
-          </div>
-          <div>
-            <label htmlFor="toDoColor">To-Do Color: </label>
-            <input className={styles.colorInput} id="toDoColor" name="toDoColor" type="color" defaultValue={"#5dc55d"}></input>
-          </div>
-          <div>
-            <label htmlFor="notesColor">Notes Color: </label>
-            <input className={styles.colorInput} id="notesColor" name="notesColor" type="color" defaultValue={color.getHex("notes")}></input>
-          </div>
-          <div>
-            <button>Apply</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+const updateSettings = (e, settings, setSettings) => {
+    const id = e.target.id;
+    const value = e.target.value;
+    const updatedSettings = {
+        ...settings,
+        [id]: value,
+    };
+    setSettings(updatedSettings);
+};
+
+export default function SettingsMenu({panelColors, setPanelColors, closeMethod, applyMethod, setSettings, settings}) {
+    return (
+        <div className={styles.fullscreen}>
+            <div className={styles.popup}>
+                <button onClick={closeMethod} className={styles.close}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    >
+                        <line x1="18" y1="6" x2="6" y2="18"></line>
+                        <line x1="6" y1="6" x2="18" y2="18"></line>
+                    </svg>
+                </button>
+                <form className={styles.form} onSubmit={(e) => updateSettings(e, settings, setSettings)}>
+                    <h2>Settings</h2>
+                    <div>
+                        <label htmlFor="favoritesColor">Favorites Color: </label>
+                        <input className={styles.colorInput} type="color" id="favoritesColor" name="favoritesColor"
+                               defaultValue={settings.favoritesColor}></input>
+                    </div>
+                    <div>
+                        <label htmlFor="emailColor">Email Color: </label>
+                        <input className={styles.colorInput} id="emailsColor" name="emailsColor" type="color"
+                               defaultValue={settings.emailsColor}></input>
+                    </div>
+                    <div>
+                        <label htmlFor="toDoColor">To-Do Color: </label>
+                        <input className={styles.colorInput} id="todoColor" name="todoColor" type="color"
+                               defaultValue={settings.todoColor}></input>
+                    </div>
+                    <div>
+                        <label htmlFor="notesColor">Notes Color: </label>
+                        <input className={styles.colorInput} id="notesColor" name="notesColor" type="color"
+                               defaultValue={settings.notesColor}></input>
+                    </div>
+                    <div>
+                        <button>Apply</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
