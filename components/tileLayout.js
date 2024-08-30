2aea5dc244aecdce460fcfca0e046584919f4d8d
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-08-29 22:03:43 -0700

    Layouts saved to localstorage

--- components/tileLayout.js
+++ components/tileLayout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Responsive, WidthProvider} from "react-grid-layout";
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
@@ -12,35 +12,57 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const TileLayout = ({settings}) => {
 
-  const layouts = {
-    lg: [
-      {i: "a", x: 0, y: 0, w: 2, h: 1},
-      {i: "b", x: 2, y: 0, w: 3, h: 1},
-      {i: "c", x: 0, y: 1, w: 3, h: 1},
-      {i: "d", x: 3, y: 1, w: 2, h: 1},
-    ],
-    md: [
-      {i: "a", x: 0, y: 0, w: 2, h: 1},
-      {i: "b", x: 2, y: 0, w: 2, h: 1},
-      {i: "c", x: 0, y: 1, w: 2, h: 1},
-      {i: "d", x: 2, y: 1, w: 2, h: 1},
-    ],
-    sm: [
-      {i: "a", x: 0, y: 0, w: 1, h: 1},
-      {i: "b", x: 1, y: 0, w: 1, h: 1},
-      {i: "c", x: 0, y: 1, w: 1, h: 1},
-      {i: "d", x: 1, y: 1, w: 1, h: 1},
-    ],
-    xs: [
-      {i: "a", x: 0, y: 0, w: 1, h: 1},
-      {i: "b", x: 0, y: 1, w: 1, h: 1},
-      {i: "c", x: 0, y: 2, w: 1, h: 1},
-      {i: "d", x: 0, y: 3, w: 1, h: 1},
-    ]
+  const defaultLayouts = {
+    lg: [{i: "a", x: 0, y: 0, w: 2, h: 1}, {i: "b", x: 2, y: 0, w: 3, h: 1}, {i: "c", x: 0, y: 1, w: 3, h: 1}, {
+      i: "d",
+      x: 3,
+      y: 1,
+      w: 2,
+      h: 1
+    },],
+    md: [{i: "a", x: 0, y: 0, w: 2, h: 1}, {i: "b", x: 2, y: 0, w: 2, h: 1}, {i: "c", x: 0, y: 1, w: 2, h: 1}, {
+      i: "d",
+      x: 2,
+      y: 1,
+      w: 2,
+      h: 1
+    },],
+    sm: [{i: "a", x: 0, y: 0, w: 1, h: 1}, {i: "b", x: 1, y: 0, w: 1, h: 1}, {i: "c", x: 0, y: 1, w: 1, h: 1}, {
+      i: "d",
+      x: 1,
+      y: 1,
+      w: 1,
+      h: 1
+    },],
+    xs: [{i: "a", x: 0, y: 0, w: 1, h: 1}, {i: "b", x: 0, y: 1, w: 1, h: 1}, {i: "c", x: 0, y: 2, w: 1, h: 1}, {
+      i: "d",
+      x: 0,
+      y: 3,
+      w: 1,
+      h: 1
+    },]
   };
 
-  return (
-    <ResponsiveGridLayout
+  const [layouts, setLayouts] = useState(null);
+  const [loadedLayout, setLoadedLayout] = useState(false);
+  useEffect(() => {
+    const storedLayout = localStorage.getItem("layouts");
+    if (storedLayout) {
+      setLayouts(JSON.parse(storedLayout));
+      setLoadedLayout(true)
+    } else {
+      setLayouts(defaultLayouts);
+      localStorage.setItem("layouts", JSON.stringify(defaultLayouts));
+      setLoadedLayout(true)
+    }
+  }, []);
+
+  useEffect(() => {
+    loadedLayout && localStorage.setItem("layouts", JSON.stringify(layouts));
+  }, [layouts]);
+
+  return (<>
+    {layouts && <ResponsiveGridLayout
       layouts={layouts}
       breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480}}
       cols={{lg: 5, md: 4, sm: 2, xs: 1}}
@@ -50,6 +72,7 @@ const TileLayout = ({settings}) => {
       preventCollision={false}
       isDraggable={settings.draggableTiles}
       resizeHandles={["se", 'sw']}
+      onLayoutChange={(layout, layouts) => setLayouts(layouts)}
     >
       <div key='a' className={styles.draggable}>
         <FavoritesPanel color={settings.favoritesColor}/>
@@ -66,8 +89,8 @@ const TileLayout = ({settings}) => {
       <div key="d">
         <NotesPanel color={settings.notesColor}/>
       </div>
-    </ResponsiveGridLayout>
-  );
+    </ResponsiveGridLayout>}
+  </>);
 };
 
 export default TileLayout;
