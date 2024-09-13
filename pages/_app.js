ecc47c7845ee76159172a67c29795de4e0d56a21
Python Darklord <pythondarklord@gmail.com>
2024-09-12 20:42:48 -0700

    Google Tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,40 @@
 import "@/styles/globals.css";
+import Script from "next/script"
+import {useEffect} from "react"
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
+const GTM_ID = "GTM-5XJ3VFM3"; // Replace with your actual GTM ID
+
+export default function MyApp({Component, pageProps}) {
+    useEffect(() => {
+        // Initialize dataLayer
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            window.dataLayer.push(arguments);
+        }
+
+        gtag("js", new Date());
+        gtag("config", GTM_ID);
+    }, []);
+
+    return (
+        <>
+            {/* Google Tag Manager */}
+            <Script
+                id="gtm-script"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+                }}
+            />
+            {/* End Google Tag Manager */}
+            <Component {...pageProps} />
+        </>
   );
 }
